c1a6c63b9dae7389e960a8e667a1c1c92d4663b8
Violeta Taseva <87774260+tasevavi@users.noreply.github.com>
2021-12-17 15:18:19 +0200

    Create day2Dive.js

---
+++ day2Dive.js
@@ -0,0 +1,20 @@
+function solve(arr) {
+    let horizontal = 0;
+    let depth = 0;
+    let aim = 0;
+    
+    for (let i = 0; i < arr.length; i++) {
+        let [command, unit] = arr[i].split(' ');
+        switch (command) {
+            case 'forward': 
+                horizontal += Number(unit);
+                depth += aim * unit;
+                break;
+            case 'down': aim += Number(unit);
+                break;
+            case 'up': aim -= Number(unit);
+                break;
+        }
+    }
+    console.log(horizontal * depth);
+}
