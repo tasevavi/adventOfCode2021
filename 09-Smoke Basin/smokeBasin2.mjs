32b61a98c60e29b765112fd6070f368ffd4d0fa9
Violeta Taseva <87774260+tasevavi@users.noreply.github.com>
2021-12-23 15:07:47 +0200

    Update smokeBasin2.mjs
    Clear logs

--- 09-Smoke Basin/smokeBasin2.mjs
+++ 09-Smoke Basin/smokeBasin2.mjs
@@ -1,23 +1,12 @@
-// const input = `2199943210
-// 3987894921
-// 9856789892
-// 8767896789
-// 9899965678`;
 import {input} from './input.mjs';
-main(input);
 
 function main(input) {
     
     let heightmap = splitInput(input);
     let lowPoints = findLowPoints(heightmap);
-    console.log('LowPoints:',lowPoints);
     let basinSizes = lowPoints.map(point => findBasinSize(heightmap, point));
-    console.log(basinSizes);
     let result = findSum(basinSizes);
     console.log('Result:', result);
-    // let basinSize = findBasinSize(heightmap, lowPoints[1]);
-    // console.log('BasinSize:',basinSize)
-
 }
 
 function findSum(basinSizes) {
@@ -31,9 +20,9 @@ function findBasinSize(heightmap, lowestPoint) {
     for (let i=0; i < visited.length; i++) {
         visited[i] = new Array(heightmap[0].length).fill(0);
     }
-    //console.log(`visited: \n${visited.join('\n')}`);
+    
     findBasinSizeRecursive(heightmap, lowestPoint, visited);
-    //console.log(`visited after fill: \n${visited.join('\n')}`);
+    
     let basinSize = visited.flat().filter(x => x === 1).length;
     return basinSize;
 }
@@ -64,21 +53,17 @@ function splitInput(input) {
     let simulateLine = new Array(heightmap[0].length).fill(9);
     heightmap.push(simulateLine);
     heightmap.unshift(simulateLine);
-    //console.log(`heightmap: \n${heightmap.join('\n')}`);
     return heightmap;
 }
 
 function findLowPoints(heightmap) {
     let lowPoints = [];
     for (let row=1; row < heightmap.length-1; row++) {
-        //console.log('CurrentLine heightmap[row]:',currentLine.join(' '));
         for (let col=1; col < heightmap[row].length-1; col++) {
 
            if (checkIsSmallest(heightmap[row][col], heightmap[row][col-1], heightmap[row][col+1], 
                 heightmap[row-1][col], heightmap[row+1][col])) {
                 lowPoints.push({row, col});
-                // console.log('row and col:', row, col);
-                // console.log('currentLevel:', currentLevel);
             }
         }
     }
@@ -88,3 +73,5 @@ function findLowPoints(heightmap) {
 function checkIsSmallest(cPos, lPos, rPos, upPos, botPos) {
     return cPos < lPos && cPos < rPos && cPos < upPos && cPos < botPos;
 }
+
+main(input);
